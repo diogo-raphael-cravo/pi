d88a40ded0dc36e18f5493b39a4ae9c8b008d331
Diogo Raphael Cravo <diogo.raphael.cravo@gmail.com>
2019-07-12 09:34:18 -0300

    Add files via upload

---
+++ docs/precache-manifest.f52a4fa5cc6bfacc2b6e485a3b1617ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94100ff9c0e19220c42bc625ebb6003f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad210ff16c9494eba49c",
+    "url": "/static/css/main.5710fa8f.chunk.css"
+  },
+  {
+    "revision": "3013d943ac37ab6f729d",
+    "url": "/static/js/2.b877121a.chunk.js"
+  },
+  {
+    "revision": "ad210ff16c9494eba49c",
+    "url": "/static/js/main.d1226383.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
